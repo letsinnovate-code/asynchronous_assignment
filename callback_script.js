35b1c8b86b0632aecf1959402f5b2b90d372bd88
letsinnovate-code <ap1106561@gmail.com>
2024-07-22 01:00:50 +0530

    callback assignment committed

---
+++ callback_script.js
@@ -0,0 +1,26 @@
+const button = document.querySelector("button");
+const main = document.querySelector(".main");
+
+button.addEventListener("click", display );
+
+
+function display(){
+
+    console.log("button clicked");
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("content")
+    main.appendChild(newDiv);
+    newDiv.innerHTML="Callback executed after 5 seconds";
+    
+    
+    
+    
+    setTimeout(() => {
+      fetch('https://jsonplaceholder.typicode.com/posts/1')
+      .then(response=>response.json())
+      .then(result=>document.querySelector(".content").textContent =`${result.title}`)
+      .catch(err=>document.querySelector(".content").textContent ="error while fetching the data")
+
+    }, 5000);
+    
+}
